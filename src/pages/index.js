1948a2598ba063072707a61a7bb6daba8d7e37b6
DevinBriscall <devinbriscall@gmail.com>
2025-01-02 09:55:34 -0500

    reroute the index link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import Link from "next/link";
 export default function Home() {
-	return <Link href={"example"}>React Introduction</Link>;
+	return <Link href={"reactBasics"}>React Introduction</Link>;
 }
