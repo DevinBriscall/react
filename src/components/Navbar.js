65e41368c109f44650826609dc25cafacee09f72
DevinBriscall <devinbriscall@gmail.com>
2025-01-24 15:22:09 -0500

    add a footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Navbar() {
 	return (
-		<div className="border-b font-bold h-[60px] px-4 flex justify-between items-center">
+		<div className="border-b sticky top-0 z-50 bg-white font-bold h-[60px] px-4 flex justify-between items-center">
 			<Link href={"/"}>React Examples</Link>
 			<div className="flex gap-2 text-blue-500 transition-all duration-300">
 				<Link href={"/react-basics"} className="hover:underline">
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Navbar />
-			<Component {...pageProps} />
+			<div className="min-h-[calc(100vh-60px)]">
+				<Component {...pageProps} />
+			</div>
+			<footer>
+				<Footer />
+			</footer>
 		</>
 	);
 }
