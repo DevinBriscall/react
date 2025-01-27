626c0d0ed346db4cdc70edaa64ebeb6c2719e57b
DevinBriscall <devinbriscall@gmail.com>
2025-01-27 18:23:56 -0500

    post banner indicating I am aware of darkmode bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,24 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 
 export default function App({ Component, pageProps }) {
+	const [showBanner, setShowBanner] = useState(true);
 	return (
 		<>
+			{showBanner && (
+				<div className="bg-red-500 p-4 flex justify-between items-center">
+					<span>I am working on a darkmode fix.</span>
+					<FontAwesomeIcon
+						icon={faClose}
+						onClick={() => setShowBanner(false)}
+						className="hover:cursor-pointer"
+					/>
+				</div>
+			)}
 			<Navbar />
 			<div className="min-h-[calc(100vh-60px)]">
 				<Component {...pageProps} />
