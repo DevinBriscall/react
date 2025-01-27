150b82ba9e4a8c5e18206edc9b5b7381d250276e
DevinBriscall <devinbriscall@gmail.com>
2025-01-27 17:43:28 -0500

    remove the navbar for now as there are too many links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,7 @@ export default function Navbar() {
 	return (
 		<div className="border-b sticky top-0 z-50 bg-white font-bold h-[60px] px-4 flex justify-between items-center">
 			<Link href={"/"}>React Examples</Link>
-			<div className="flex gap-2 text-blue-500 transition-all duration-300">
-				<Link href={"/react-basics"} className="hover:underline">
-					React Basics
-				</Link>
-				<Link href={"/data-fetching"} className="hover:underline">
-					Data Fetching
-				</Link>
-				<Link href={"/flex-card"} className="hover:underline">
-					Flex
-				</Link>
-				<Link href={"/http"} className="hover:underline">
-					HTTP
-				</Link>
-				<Link href={"/strapi"} className="hover:underline">
-					strapi
-				</Link>
-			</div>
+			<div className="flex gap-2 text-blue-500 transition-all duration-300"></div>
 		</div>
 	);
 }
