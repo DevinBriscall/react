e193c71ca4948fabf12ceede92d656b362bfc69b
DevinBriscall <devinbriscall@gmail.com>
2025-01-28 12:05:44 -0500

    add my social media links to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,9 @@
+import {
+	faGithubSquare,
+	faLine,
+	faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
 
@@ -5,7 +11,23 @@ export default function Navbar() {
 	return (
 		<div className="border-b sticky top-0 z-50 bg-white font-bold h-[60px] px-4 flex justify-between items-center">
 			<Link href={"/"}>React Examples</Link>
-			<div className="flex gap-2 text-blue-500 transition-all duration-300"></div>
+			<div className="flex items-center gap-2 transition-all duration-300">
+				<span>Devin Briscall:</span>
+				<a
+					href={"https://github.com/DevinBriscall"}
+					target="_blank"
+					className="hover:text-blue-500 flex items-center transition-all duration-300"
+				>
+					<FontAwesomeIcon icon={faGithubSquare} className="w-7 h-7" />
+				</a>
+				<a
+					href={"https://www.linkedin.com/in/devin-briscall-933843271/"}
+					target="_blank"
+					className="hover:text-blue-500 flex items-center transition-all duration-300"
+				>
+					<FontAwesomeIcon icon={faLinkedin} className="w-7 h-7" />
+				</a>
+			</div>
 		</div>
 	);
 }
